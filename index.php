91c12410d7249b705fa8a899946d9a85742ef23b
heksen84 <heksen84@yandex.ru>
2018-03-04 21:06:51 +0600

    ok

--- index.php
+++ index.php
@@ -17,19 +17,8 @@
     <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
     <b-collapse is-nav id="nav_dropdown_collapse">
       <b-navbar-nav>
-        <b-nav-item href="#">Home</b-nav-item>
-        <b-nav-item href="#">Link</b-nav-item>
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-        <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="#">Account</b-dropdown-item>
-          <b-dropdown-item href="#">Settings</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <b-nav-item href="/"><b>DreamSite</b></b-nav-item>
+        <b-nav-item href="/about">Link</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
