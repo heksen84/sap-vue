6cc5d8f2a7d345f829696436a563a1f61ea486f2
heksen84 <heksen84@yandex.ru>
2018-03-04 21:12:21 +0600

    ok

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <meta charset = "utf-8">
 <meta name = "viewport"	content	= "width=device-width"/>
-<meta name = "description" content = "литературный ресурс"/>
+<meta name = "description" content = "DreamSite - сайты на заказ любой сложности"/>
 <meta name = "keywords"	content = "Сделать сайт, Заказать сайт, Сайт на заказ, Нужен сайт, Сайт для, Разработать сайт, Хочу сайт, Заказать лендинг, Нужен интернет-магазин, Заказать интернет магазин"/>
 <meta name = "robots" content = "index, follow"/>
 <meta name = "viewport"	content = "width=device-width, initial-scale=1, shrink-to-fit=no">
